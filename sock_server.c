3cd9d6e0b5a9bac6f19924da559c8af634641106
Faical Bouibaouen <fbouibao@e1r12p10.1337.ma>
2022-04-13 21:40:18 +0000

    backup

--- sock_server.c
+++ sock_server.c
@@ -5,15 +5,39 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <fcntl.h>
 
-#define PORT 8080
+#define PORT 999
+int receive_basic(int s)
+{
+	int size_recv , total_size= 0;
+	char chunk[600000];
+	
+	//loop
+	while(1)
+	{
+		memset(chunk ,0 , 600000);	//clear the variable
+		if((size_recv =  recv(s , chunk , 600000 , 0) ) < 0)
+		{
+			break;
+		}
+		else
+		{
+			total_size += size_recv;
+			 int out = open("data222", O_RDWR | O_CREAT | O_TRUNC, 777);
+            write(out, chunk, 600000);
+		}
+	}
+	
+	return total_size;
+}
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket; long valread;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     
-    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    char *hello = "HTTP/1.1 200 OK\nContent-Type: image/png\nContent-Type: text/html\nContent-Length: 700\n\n<h1>Hello world!</h1><img src='/Users/fbouibao/Desktop/webserv/icons8-home-32.png' alt=''>";
     
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -40,18 +64,30 @@ int main(int argc, char const *argv[])
         perror("In listen");
         exit(EXIT_FAILURE);
     }
+    int i = 0;
     while(1)
     {
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
         {
             perror("In accept");
-            exit(EXIT_FAILURE);
+            close(new_socket);
+            //exit(EXIT_FAILURE);
         }
         
-        char buffer[30000] = {0};
-        valread = read( new_socket , buffer, 30000);
-        printf("%s\n",buffer );
+        char *buffer = malloc(sizeof(char) * 600000);
+        // valread = read( new_socket , buffer, 600000);
+        int bytes = recv(new_socket, buffer, 600000, 0);
+        // receive_basic(new_socket);
+        char *suffix = malloc(2);
+        suffix[1] = 0;
+        suffix[0] = i + 48;
+        // char *filename = malloc(20);
+        // asprintf(&filename, "%s%s", "./data", suffix);
+        // printf("file: %s\n", filename);
+       
+        i++;
+        printf("reached\n");
         write(new_socket , hello , strlen(hello));
         printf("------------------Hello message sent-------------------\n");
         close(new_socket);
