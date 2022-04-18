e9d1a6fe128ed1d75aa8c3fea0017bef3f4a9514
Faical Bouibaouen <fbouibao@e1r12p10.1337.ma>
2022-04-18 23:20:36 +0000

    backup

--- sock_server.c
+++ sock_server.c
@@ -7,26 +7,33 @@
 #include <string.h>
 #include <fcntl.h>
 
-#define PORT 999
+#define PORT 9091
 int receive_basic(int s)
 {
-	int size_recv , total_size= 0;
-	char chunk[600000];
-	
+	int size_recv = 0 , total_size= 0;
+	char *chunk = malloc(500);
+	int i = 0;
+    int count = 0;
 	//loop
-	while(1)
+    int out = open("data222.txt", O_WRONLY | O_APPEND | O_TRUNC);
+	while(i<1)
 	{
-		memset(chunk ,0 , 600000);	//clear the variable
-		if((size_recv =  recv(s , chunk , 600000 , 0) ) < 0)
+		memset(chunk ,0 , 500);	//clear the variable
+       recv(s , chunk , 500, 0);
+        printf("===>%d\n", size_recv);
+		if((size_recv ) < 0)
 		{
-			break;
+            printf("-----> %d\n", size_recv);
+			return (0);
 		}
 		else
 		{
+            printf("-----> %d\n", size_recv);
 			total_size += size_recv;
-			 int out = open("data222", O_RDWR | O_CREAT | O_TRUNC, 777);
-            write(out, chunk, 600000);
+			
+            write(out, chunk, 500);
 		}
+        i++;
 	}
 	
 	return total_size;
@@ -36,7 +43,7 @@ int main(int argc, char const *argv[])
     int server_fd, new_socket; long valread;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    
+ 
     char *hello = "HTTP/1.1 200 OK\nContent-Type: image/png\nContent-Type: text/html\nContent-Length: 700\n\n<h1>Hello world!</h1><img src='/Users/fbouibao/Desktop/webserv/icons8-home-32.png' alt=''>";
     
     // Creating socket file descriptor
@@ -49,11 +56,12 @@ int main(int argc, char const *argv[])
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
+    
     address.sin_port = htons( PORT );
     
     memset(address.sin_zero, '\0', sizeof address.sin_zero);
-    
-    
+    // fcntl()
+    fcntl(server_fd, F_SETFL, O_NONBLOCK);
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
     {
         perror("In bind");
@@ -65,32 +73,64 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
     int i = 0;
+    fd_set current_sockets, ready_sockets;
+    FD_ZERO(&current_sockets);
+    FD_SET(server_fd, &current_sockets);
     while(1)
     {
+        ready_sockets = current_sockets;
         printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        if (select(FD_SETSIZE, &ready_sockets, NULL, NULL, NULL) < 0)
         {
-            perror("In accept");
-            close(new_socket);
-            //exit(EXIT_FAILURE);
+            perror("select error");
+            exit(EXIT_FAILURE);
+        }
+        int client_socket;
+        for (size_t i = 0; i < FD_SETSIZE; i++)
+        {
+            if (FD_ISSET(i, &ready_sockets))
+            {
+                if(i == server_fd)
+                {
+                    client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+                    
+                    FD_SET(client_socket, &current_sockets);
+                    receive_basic(client_socket);
+
+                    write(client_socket , hello , strlen(hello));
+                    printf("------------------Hello message sent-------------------\n");
+                    close(client_socket);
+                }
+            }
+            else
+            {
+                FD_CLR(i, &current_sockets);
+            }
+
         }
         
-        char *buffer = malloc(sizeof(char) * 600000);
-        // valread = read( new_socket , buffer, 600000);
-        int bytes = recv(new_socket, buffer, 600000, 0);
-        // receive_basic(new_socket);
-        char *suffix = malloc(2);
-        suffix[1] = 0;
-        suffix[0] = i + 48;
-        // char *filename = malloc(20);
-        // asprintf(&filename, "%s%s", "./data", suffix);
-        // printf("file: %s\n", filename);
+        
+        // if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        // {
+        //     perror("In accept");
+        //     close(new_socket);
+        //     //exit(EXIT_FAILURE);
+        // }
+        
+        // char *buffer = malloc(sizeof(char) * 1000);
+        // char *buffer2 = malloc(sizeof(char) * 1000);
+        // int fda = open("data", O_CREAT | O_RDWR | O_TRUNC | O_APPEND, 777);
+        // // valread = read( server_fd , buffer2, 1000);
+        // // buffer2[999] = '\0';
+        // // write(fda , buffer2 , strlen(buffer2));
+        // buffer[999] = '\0';
+
+        // int bytes = recv(new_socket, buffer, 1000, 0);
+        // receive_basic(client_socket);
+
        
-        i++;
-        printf("reached\n");
-        write(new_socket , hello , strlen(hello));
-        printf("------------------Hello message sent-------------------\n");
-        close(new_socket);
+       
+
     }
     return 0;
 }
@@ -99,3 +139,12 @@ int main(int argc, char const *argv[])
 
 
 
+
+
+// int main()
+// {
+//     fcntl(0, F_SETFL, O_NONBLOCK);
+//     char *
+//      printf("%d\n", read(0, NULL, 10));
+//      return 0;
+// }
